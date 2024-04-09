6c66ed22f01c71c6d28373502f3babfd74135368
shubhPawar-Rajput <shubhpawar1993@gmail.com>
2024-04-09 23:24:59 +0530

    AppWrite Storage Create File Service

--- BlopPostAPP/src/appwrite/config.js
+++ BlopPostAPP/src/appwrite/config.js
@@ -11,7 +11,7 @@ export class Service {
       .setEndpoint(conf.appwriteUrl)
       .setProject(conf.appwriteProjectId);
     this.databases = new Databases(this.client);
-    this.bucket = new Databases(this.client);
+    this.bucket = new Storage(this.client);
   }
 
   async createPost({ title, slug, content, featuredImage, status, userId }) {
@@ -94,6 +94,20 @@ export class Service {
       return false;
     }
     return null;
+  }
+
+  //file upload service
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBuckettId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("appwrite upload file  error :: ", error);
+      return false;
+    }
   }
 }
 
